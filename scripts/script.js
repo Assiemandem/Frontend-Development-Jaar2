07d21a88a7c4418e04b5f30bb9d09cd7fa2f60e2
Assiemandem <vincentvanzeelt@live.nl>
2024-09-16 12:31:20 +0200

    snelle_update

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,27 @@
 // JavaScript Document
 console.log("hi");
+
+
+//OPENEN//
+var openButton = document.querySelector("header > button");
+openButton.onclick = openMenu;
+
+
+function openMenu() {  
+  var deNav = document.querySelector("nav");
+  deNav.classList.add("toonMenu");
+}
+
+
+
+
+ //SLUITEN//
+var sluitButton = document.querySelector("nav button");
+
+sluitButton.onclick = sluitMenu;
+
+function sluitMenu() {
+  var deNav = document.querySelector("nav");
+  deNav.classList.remove("toonMenu");
+}
+
